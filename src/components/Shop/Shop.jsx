5bf53cb3bc16fc57b6b14bc04b8ce4ef6ff88b09
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-09 01:45:29 +0600

    basic product added and display them

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { products } from '../../data';
 
 const Shop = () => {
   return (
-    <div>
+    <div className='container mx-auto px-5'>
       <h2>This is shop page</h2>
+
+
+      {/* Display Products */}
+      <div className="w-full mt-2 overflow-x-auto">
+        <table className="table-auto text-left w-full">
+          <thead className="bg-[#f7f7f7]">
+            <tr>
+              <th className="p-3 text-sm font-semibold text-left pl-10">
+                Image
+              </th>
+              <th className="p-3 text-sm font-semibold text-left">
+                Name
+              </th>
+              <th className="p-3 text-sm font-semibold text-left">
+                Color
+              </th>
+              <th className="p-3 text-sm font-semibold text-left">
+                Stock
+              </th>
+              <th className="p-3 text-sm font-semibold text-left">
+                Price
+              </th>
+              <th className="p-3 text-sm font-semibold text-left">
+                Buy
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product, index) => {
+              return (
+                <tr key={index} className="">
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    <img width={100} src={product?.image} alt="img" />
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {product.name}
+                  </td>
+
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {product.color}
+                  </td>
+
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {product.availableQuantity > 0 ? (
+                      <p>In Stock</p>
+                    ) : (
+                      <p>Out Of Stock</p>
+                    )}
+                  </td>
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {product.price}
+                  </td>
+
+                  <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                    {product.availableQuantity}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,94 @@
+import hoodie_black from './assets/hoodie_black.jpg'
+
+export const products = [
+  {
+    id: 1,
+    name:'Woo Ninja',
+    image: hoodie_black,
+    price: 35,
+    size:'xl',
+    color: 'red',
+    availableQuantity: 12,
+    category: 'hoodie'
+  },
+  {
+    id: 2,
+    name:'Woo Logo',
+    image: hoodie_black,
+    price: 40,
+    size:'xxl',
+    color: 'green',
+    availableQuantity: 21,
+    category: 'hoodie'
+  },
+  {
+    id: 3,
+    name:'Hoodie C',
+    image: hoodie_black,
+    price: 55,
+    size:'xxl',
+    color: 'white',
+    availableQuantity: 0,
+    category: 'hoodie'
+  },
+  {
+    id: 4,
+    name:'Hoodie B',
+    image: hoodie_black,
+    price: 60,
+    size:'xl',
+    color: 'black',
+    availableQuantity: 8,
+    category: 'hoodie'
+  },
+  {
+    id: 5,
+    name:'pant l',
+    image:'',
+    price: 40,
+    size:'lg',
+    color: 'black',
+    availableQuantity: 7,
+    category: 'pant'
+  },
+  {
+    id: 6,
+    name:'tshirt',
+    image:'',
+    price: 30,
+    size:'lg',
+    color: 'red',
+    availableQuantity: 5,
+    category: 'shirt'
+  },
+  {
+    id: 7,
+    name:'Hoodie M',
+    image:'',
+    price: 70,
+    size:'lg',
+    color: 'gray',
+    availableQuantity: 6,
+    category: 'hoodie'
+  },
+  {
+    id: 8,
+    name:'zeens',
+    image:'',
+    price: 50,
+    size:'xl',
+    color: 'black',
+    availableQuantity: 7,
+    category: 'pant'
+  },
+  {
+    id: 9,
+    name:'shirt',
+    image:'',
+    price: 40,
+    size:'lg',
+    color: 'black',
+    availableQuantity: 0,
+    category: 'shirt'
+  },
+]
